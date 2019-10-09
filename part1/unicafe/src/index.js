10f945247587b9c099ef5ab21d49d9344e1b9cba
BoushraKolko <bushrakolko@hotmail.com>
2019-10-09 19:17:34 +0200

    unicafe exercise step 2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,20 +6,58 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [avarage, setAvarage] = useState(0)
+  const goodClick =()=>{
+    
+    setTotal(total+1)
+    setAvarage(avarage+1)
+    console.log(total)
+    return setGood(good+1)
+  }
+
+  
+  const neutrallClick =()=>{
+    
+    setTotal(total+1)
+    setAvarage(avarage+0)
+    return setNeutral(neutral+1)
+  }
+
+  
+  const badClick =()=>{
+    
+    setTotal(total+1)
+    setAvarage(avarage-1)
+    return setBad(bad+1)
+  }
+
+  const showAvarage =()=>{
+
+    if (avarage===0) return <div>Avarage :0</div>
+    else return <div>Avarage :{avarage/total}</div>
+  }
+
+  const showPositive=()=>{
+
+    if (total===0) return <div>Positive :0</div>
+    else return <div>Positive :{(good/total)*100} %</div>
+
+  }
 
   return (
     <div>
      <h1>Give feedback</h1>
      <br/>
-     <button onClick={()=>setGood(good+1)}>
+     <button onClick={goodClick}>
        Good
        </button> 
        
-     <button onClick={()=>setNeutral(neutral+1)}>
+     <button onClick={neutrallClick}>
        Neutral
        </button>
        
-     <button onClick={()=>setBad(bad+1)}>
+     <button onClick={badClick}>
        Bad
        </button>
        <h1>Statistics</h1>
@@ -27,6 +65,10 @@ const App = () => {
        Good : {good}<br/>
        Neutral : {neutral}<br/>
        Bad :  {bad}<br/>
+        {showAvarage()}
+       All : {total}<br/>
+       {showPositive()}
+
     </div>
   )
 }
