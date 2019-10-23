0495daff8de6b86a218e6acfdf2e9a0ebafe4c4b
BoushraKolko <bushrakolko@hotmail.com>
2019-10-23 18:10:57 +0200

    exercises 2.1 and 2.2 and 2.3 added

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -18,19 +18,19 @@ const Part = (props) => {
 
 }
 
-const Content = (props) => 
-    props.parts.map(content => 
-      <Part key={content.id} part={content.name} exercises={content.exercises} />  
-  
+const Content = (props) =>
+  props.parts.map(content =>
+    <Part key={content.id} part={content.name} exercises={content.exercises} />
+
   )
 
 
 const Total = (props) => {
 
-  const reducer =(total,current)=> total+current
+  const reducer = (total, current) => total + current
   return (
     <div>
-      <h1>Number of exercises {props.num.map(parts=>parts.exercises).reduce(reducer)}</h1>
+      <h1>Number of exercises {props.num.map(parts => parts.exercises).reduce(reducer)}</h1>
     </div>
   )
 }
@@ -41,17 +41,17 @@ const App = () => {
     name: 'Half Stack application development',
     parts: [
       {
-        id:1,
+        id: 1,
         name: 'Fundamentals of React',
         exercises: 10
       },
       {
-        id:2,
+        id: 2,
         name: 'Using props to pass data',
         exercises: 7
       },
       {
-        id:3,
+        id: 3,
         name: 'State of a component',
         exercises: 14
       }
