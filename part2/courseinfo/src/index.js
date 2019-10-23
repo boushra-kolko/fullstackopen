f556b7215dad60b0bece2f2a0201eafc1b59f984
BoushraKolko <bushrakolko@hotmail.com>
2019-10-23 18:08:47 +0200

    exercises 2.1 and 2.2 added

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -13,7 +13,6 @@ const Header = (props) => {
 
 const Part = (props) => {
   return (<div>
-
     <p>{props.part} {props.exercises}</p>
   </div>)
 
@@ -21,7 +20,7 @@ const Part = (props) => {
 
 const Content = (props) => 
     props.parts.map(content => 
-      <Part part={content.name} exercises={content.exercises} />  
+      <Part key={content.id} part={content.name} exercises={content.exercises} />  
   
   )
 
@@ -31,7 +30,7 @@ const Total = (props) => {
   const reducer =(total,current)=> total+current
   return (
     <div>
-      <p>Number of exercises {props.num.map(parts=>parts.exercises).reduce(reducer)}</p>
+      <h1>Number of exercises {props.num.map(parts=>parts.exercises).reduce(reducer)}</h1>
     </div>
   )
 }
@@ -42,14 +41,17 @@ const App = () => {
     name: 'Half Stack application development',
     parts: [
       {
+        id:1,
         name: 'Fundamentals of React',
         exercises: 10
       },
       {
+        id:2,
         name: 'Using props to pass data',
         exercises: 7
       },
       {
+        id:3,
         name: 'State of a component',
         exercises: 14
       }
@@ -58,7 +60,6 @@ const App = () => {
   return (
     <div>
       <Header courseName={course.name} />
-
       <Content parts={course.parts} />
       <Total num={course.parts} />
 
