b0bf6dcd0f9332709c97070b0778b5846b7210f5
BoushraKolko <bushrakolko@hotmail.com>
2019-10-23 10:42:16 +0200

    exersise 1.12 added

--- part1/anecdots/src/index.js
+++ part1/anecdots/src/index.js
@@ -3,49 +3,35 @@ import ReactDOM from 'react-dom'
 
 
 const App = (props) => {
-    const [selected, setSelected] = useState(0)
-    let voteArray = Array.apply(null, new Array(props.anecdotes.length)).map(Number.prototype.valueOf, 0);
-    const [voted, setVoted] = useState([...voteArray])
-    let rand = 0
-
-
-    const showRandom = () => {
-        rand = Math.random()
-        rand = rand * 25
-        rand = Math.floor(rand)
-        let leng = props.anecdotes.length
-        if (rand >= leng) rand = 3
-         setSelected(rand)
-    }
-
-    const vote = () => {
-        voteArray = [...voted]
-        voteArray[selected] = voted[selected] + 1
-        setVoted(voteArray)
-
-
-    }
-
-
-    return (
-        <div>
-            <div> {props.anecdotes[selected]} <br/>selected {voted[selected]} times
-     </div>      <br /><button onClick={showRandom}>Next amecdots</button>
-            <br></br><button onClick={vote}>Vote</button>
-        </div>
-    )
+  const [selected, setSelected] = useState(0)
+    const showRandom=()=>{
+    let rand=0
+    rand=Math.random()
+    rand=rand*25
+    rand=Math.floor(rand)
+   let leng=props.anecdotes.length
+  if(rand>=leng) rand=3  
+   console.log(rand)
+    setSelected(rand)
+}
+  return (
+    <div>
+      {props.anecdotes[selected]}
+      <br/><button onClick={showRandom}>Next amecdots</button>
+    </div>
+  )
 }
 
 const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
 ReactDOM.render(
-    <App anecdotes={anecdotes} />,
-    document.getElementById('root')
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
 )
