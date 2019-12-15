069f4d71130c9c01c76001343e57cc264234b190
BoushraKolko <bushrakolko@hotmail.com>
2019-12-15 23:14:11 +0100

    2.13 added

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -7,32 +7,36 @@ import './App.css';
 function App() {
   const [country, setCountry] = useState('')
   const DataList = Connect()
-
   const stopSubmit = (event) => { event.preventDefault() }
   const changeCountry = (event) => { setCountry(event.target.value) }
-  const showCountries = () => {
+  const showCountry = (event) => {
+    setCountry(event.target.name)
+    showCountries(event.target.name.toLowerCase().trim())}
+  const showCountries = (filteredCountry = '') => {
     let x = []
-    if (DataList.length > 0 && country.length > 0) {
-      let filteredCountry = country.trim().toLowerCase()
+    if (DataList.length > 0 ) {
+      
       x = DataList.filter(element => element.name.trim().toLowerCase().includes(filteredCountry))
+      
       if (x.length > 10) return <div>
         Too many matches, specify another filter.
    </div>
 
-      else if (x.length == 1) return <div>
+      else if (x.length === 1) {
+        return <div>
         <h3>{x[0].name}</h3><br /><br />
         Capital: {x[0].capital}<br />
         Population: {x[0].population}<br />
         <h4>Langauges: </h4>{x[0].languages.map(language => { return <div key={language.name}> {language.name}<br /></div> })}
         <img src={x[0].flag} height='100' width='100' ></img>
-      </div>
+      </div>}
       else if (x.length == 0) return <div>
         No matches found.
   </div>
 
       else return <div>
         {x.map(country => {
-          return <div key={country.name}> {country.name}<br /></div>
+          return <div key={country.name}> {country.name}<button onClick={showCountry} key={country.name} name={country.name}>Show</button><br /></div>
         })}
       </div>
     }
@@ -44,7 +48,7 @@ function App() {
       <form onSubmit={stopSubmit}>
         Find countries: <input type='text' value={country} onChange={changeCountry}></input>
       </form>
-      {showCountries()}
+      {showCountries(country.trim().toLowerCase())}
     </div>
   );
 }
